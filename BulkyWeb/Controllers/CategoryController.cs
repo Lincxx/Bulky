884923397dfb772763056ac4fac5d1cfaacfacac
lincxx <bossc@BOSSCO>
2023-04-07 08:38:09 -0400

    Added in Server vaildation, Adding to DB via Entity Framework

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //In a field declaration, readonly indicates that assignment to the field can only occur as part of the
+        //declaration or in a constructor in the same class. A readonly field can be assigned and reassigned
+        //multiple times within the field declaration and constructor
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //var objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();           
+        }
+    }
+}
